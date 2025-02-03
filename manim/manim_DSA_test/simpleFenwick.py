fa3935a354e18e86651efc39412592ac4e289c00
August-Brandt <augustbrandt170@gmail.com>
2025-02-03 18:27:03 +0100

    Add beginings of a simple fenwick animation

---
+++ manim/manim_DSA_test/simpleFenwick.py
@@ -0,0 +1,69 @@
+from manim import *
+from manim_dsa import *
+
+class Test(Scene):
+    def construct(self):
+        array = [4,8,5,2,6,1,0,8]
+        UPLEVEL = 1.3
+        startArray = (
+            MArray(array, style=MArrayStyle.BLUE)
+            .add_indexes()
+            .scale(0.7)
+        )
+        
+        boxes = []
+        for num in array:
+            box = Square(1)
+            if len(boxes) <= 0:
+                box.to_edge(DOWN).to_edge(LEFT)
+            else:
+                box.next_to(boxes[-1][0])
+            numText = (
+                Text(str(num), stroke_width=3)
+                .move_to(box.get_center())
+            )
+            boxes.append((box, numText))
+            self.play(Create(box), Create(numText), run_time=0.5)
+
+        self.moveNumBox(boxes[1], UP, UPLEVEL)
+
+        newLevel = (
+            Rectangle(height=1, width=2.27) # 2.27 is the width of 2 square with the gap
+            .to_edge(LEFT)
+            .to_edge(DOWN)
+            .shift(UP*UPLEVEL)
+        )
+
+        self.play(
+            Transform(boxes[1][0], newLevel), 
+            Transform(boxes[1][1], Text("12", stroke_width=3).move_to(newLevel.get_center())),
+            rate_func=linear,
+            run_time=0.6
+        )
+
+        newNewLevel = (
+            Rectangle(height=1, width=2.27*2) # 2.27 is the width of 2 square with the gap
+            .to_edge(LEFT)
+            .to_edge(DOWN)
+            .shift(UP*UPLEVEL*2)
+        )
+
+        self.moveNumBox(boxes[3], UP, UPLEVEL*2)
+
+        self.play(
+            Transform(boxes[3][0], newNewLevel), 
+            Transform(boxes[3][1], Text("19", stroke_width=3).move_to(newNewLevel.get_center())),
+            rate_func=linear,
+            run_time=0.75
+        )
+
+        # self.play(Create(startArray), run_time=3)
+        # self.play(startArray.animate.to_edge(UP))
+        # self.play(Create(box1), Create(num1))
+        self.wait(1)
+    
+    def moveNumBox(self, box, direction, amount):
+        self.play(
+            box[0].animate.shift(direction*amount),
+            box[1].animate.shift(direction*amount)
+        )
