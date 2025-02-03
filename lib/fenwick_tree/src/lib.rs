390c64efb3853bb944a1d602808a42acd5408472
Slug-Boi <github.unshipped609@slmail.me>
2025-02-03 16:37:16 +0100

    update rust fenwick code to have index check
    this is to ensure that range calls are in bounds

--- lib/fenwick_tree/src/lib.rs
+++ lib/fenwick_tree/src/lib.rs
@@ -1,10 +1,5 @@
 use pyo3::prelude::*;
-
-/// Formats the sum of two numbers as string.
-// #[pyfunction]
-// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-//     Ok((a + b).to_string())
-// }
+use rayon::{option, prelude::*};
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -21,31 +16,18 @@ fn fenwick_tree(m: &Bound<'_, PyModule>) -> PyResult<()> {
 
 // Important note here is that the tree is 1 indexed, so the first element is at index 1
 // BUT when making range or sum calls the input index is 0 indexed so a tree of size 5 will have valid indices 0, 1, 2, 3, 4
-// fn main() {
-//     let n = 5;
-
-//     let mut tree = FenwickTree::new(n);
-
-//     let sum = tree.sum(3);
-
-//     //dbg!(&tree.tree);
-
-//     println!("Sum of first 4 elements is {}", sum);
-
-//     tree.update(2, 10);
-
-//     let sum = tree.sum(3);
-
-//     // dbg!(&tree.tree);
-
-//     println!("Sum of first 4 elements after update is {}", sum);
-
-// }
+fn index_check(index: i32, size: usize) {
+    if index >= 0 || index < size as i32 {
+        return
+    }
+    panic!("Index out of bounds {}, size {}", index, size);
+}
 
 // Fenwick tree is represented using a Vector
 #[pyclass]
 struct FenwickTree {
     tree: Vec<i32>,
+    size: i32
 }
 
 #[pymethods]
@@ -54,6 +36,7 @@ impl FenwickTree {
     fn update(&mut self, mut index: i32, value: i32) {
         // 1 indexed cause that just how it be
         index = index + 1;
+        index_check(index, self.tree.len());
 
         while index <= (self.tree.len()-1) as i32 {
             self.tree[index as usize] += value;
@@ -64,10 +47,11 @@ impl FenwickTree {
     // This function behaves more like what you would expect from an update function where it overides the value at index
     // And then propagates the change up the tree. NOTE: Be careful as this may result in negative values in the tree
     // so please exercise caution when using this
-    // TODO: add failsafe to prevent negative values
     fn override_update(&mut self, mut index: i32, value: i32) {
+        
         // 1 indexed cause that just how it be
         index = index + 1;
+        index_check(index, self.tree.len());
 
         let diff = value - self.tree[index as usize];
 
@@ -81,7 +65,7 @@ impl FenwickTree {
     fn new(size: i32) -> Self {
         let mut tree = vec![0; (size + 1) as usize];
         tree[0] = -9999; // Placeholder for 1-indexed tree
-        let mut fenwick_tree = FenwickTree { tree };
+        let mut fenwick_tree = FenwickTree { tree, size };
         for x in 0..size {
             // dbg!(x);
             fenwick_tree.update(x, x+1);
@@ -99,6 +83,17 @@ impl FenwickTree {
             sum += self.tree[index as usize];
             index -= index & -index;
         }
+
+        // sum = self.tree
+        //     .par_iter().step_by((index & -index) as usize)
+        //     .filter_map(|&i| if i > 0 {Some(self.tree[i as usize])} else {None})
+        //     .sum();
+
+        //Parallelize the sum calculation using Rayon
+        // sum = (1..=index).collect::<Vec<_>>()
+        //     .into_par_iter().step_by((index & -index) as usize)
+        //     .filter_map(|i| if i > 0 {Some(self.tree[(i) as usize])} else {None})
+        //     .sum();
 
         sum
     }
