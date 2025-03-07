9b952f007d14fbd705c3737907ffdd484fe6631f
Slug-Boi <github.unshipped609@slmail.me>
2025-03-07 15:31:46 +0100

    create initial 2d fenwick tree animation create box tree class for 1d BITS
    Co-authored-by: August-Brandt <augustbrandt170@gmail.com>

--- manim/2dFenwickTree/2dFenwickTree.py
+++ manim/2dFenwickTree/2dFenwickTree.py
@@ -2,40 +2,43 @@ from twoDBit import TwoDBIT
 from manim import *
 from box_tree import *
 from fenwick_tree import FenwickTree
+from copy import deepcopy
 
 class TwoDFenwick(Scene):
     def construct(self):
         M2 = [
-                [3,  4,  0],
-                [8,  11, 10],
-                [9,  7,  5],
+                [3,  4,  0, 2],
+                [8,  11, 10, 3],
+                [9,  7,  5, 8],
+                [6,  2,  1, 2],
             ]
-        bit = TwoDBIT(M2, 3)
+        bit = TwoDBIT(M2, 4)
         operations = bit.CreatePositions()
         bit.PrintBit()
 
         test = [2, 1, 4, 1, 0, 8, 10, 2]
 
-        treeStyle = {
-            "rect": {
-                "height": 0.5,
-                "width": 0.5,
-                "color": BLUE,
-            },
-            "value": {},
-            "buffer": 0,
-            "uplevel": 0.5,
-        }
+        small_tree_style = deepcopy(BoxTree.defaultStyle)
+        small_tree_style["rect"]["height"] = 0.25
+        small_trees = VGroup()
+        for i in range(len(M2)):
+            small_trees.add(BoxTree([0 for _ in range(len(M2[i]))], style=small_tree_style, show_indices=True, show_values=False).scale(0.5))
 
-        tempTree = BoxTree(test)
-        self.play(Create(tempTree))
-        self.play(*tempTree.create())
+        small_trees.arrange(UP, buff=1)
+
+        self.play(Create(small_trees,lag_ratio=0.2))
+        self.wait(1)
+        self.play(*[tree.create() for tree in small_trees])
         self.wait(2)
 
-        # m1 = Matrix(M2)
-        # self.play(Create(m1))
-        # self.wait(2)
-        # self.play(Uncreate(m1))
-        # bitMobject = Matrix(bit.getBIT())
-        # self.play(Create(bitMobject))
-        # self.wait(2)
+        big_tree_style = deepcopy(BoxTree.defaultStyle)
+        big_tree_style['x_buffer'] = (small_trees[1].get_center() - small_trees[0].get_center())[1] - big_tree_style["rect"]["height"] + small_trees[0][0][2].get_height()/3
+        #big_tree_style['y_buffer'] = 0.4
+        big_tree_style["y_buffer"] = 0.4
+
+        #TODO: add a create function and change current create to animate_create in box_tree module
+        big_tree = BoxTree([0 for _ in range(len(M2))], show_indices=True, show_values=False, style=big_tree_style)
+        self.play(Create(big_tree))
+        self.play(big_tree.create())
+        self.add(big_tree.rotate(PI/2).move_to(small_trees.get_corner(DL),DR).shift(LEFT*0.2))
+        self.wait(2)
--- manim/2dFenwickTree/box_tree.py
+++ manim/2dFenwickTree/box_tree.py
@@ -1,28 +1,55 @@
 from manim import *
+from manim_dsa import Highlightable
 from math import log2
 from fenwick_tree import FenwickTree
+from typing import override
 
-class TreeElement(VGroup):
-    def __init__(self, rect, value):
+class TreeElement(VGroup, Highlightable):
+    def __init__(self, rect, value, index=None):
         super().__init__()
         self.rect = rect
+        self._add_highlight(self.rect)
         self.value = value.move_to(self.rect)
         self += self.rect
         self += self.value
-
+        if index:
+            self.index = index.move_to(self.rect.get_bottom(), UP).shift(DOWN*0.1)
+            self += self.index
+    
 class BoxTree(VGroup):
-    def __init__(self, array, rectHeight=0.5, uplevel=None, buffer=0):
+    defaultStyle = {
+            "rect": {
+                "height": 0.5,
+                "width": 0.5,
+                "color": BLUE_A,
+                "fill_color": BLUE,
+                "fill_opacity": 1.0
+            },
+            "index": {
+                "color": WHITE,
+            },
+            "value": {},
+            "x_buffer": 0,
+            "y_buffer": 0,
+            "value_scale": 0.4,
+            "index_scale": 0.4,
+            "uplevel": 0.5,   
+}
+    def __init__(self, array, style=defaultStyle, show_indices=False, show_values=True):
         super().__init__()
-        self.buffer = buffer
         self.array = array
-        self.rectHeight = rectHeight
-        if uplevel:
-            self.uplevel = uplevel
-        else:
-            self.uplevel = rectHeight
-        for v in self.array:
-            self += TreeElement(Rectangle(height=rectHeight, width=rectHeight, color=BLUE), Text(str(v), weight=SEMIBOLD).scale(0.4))
-        self.arrange(buff=self.buffer)
+        self.style = style
+        self.show_values = show_values
+        self.show_indices = show_indices
+        for i,v in enumerate(self.array):
+            self += (
+                TreeElement(
+                    Rectangle(**self.style["rect"]),
+                    Text(str(v), **self.style["value"], fill_opacity=1 if self.show_values else 0).scale(self.style["value_scale"]),
+                    index=Text(str(i+1), **self.style["index"]).scale(self.style["index_scale"]) if self.show_indices else None
+                    )
+            )
+        self.arrange(buff=self.style["x_buffer"])
         self.move_to(ORIGIN)
     
     @staticmethod
@@ -33,24 +60,34 @@ class BoxTree(VGroup):
     def create(self):
         tree = FenwickTree(self.array)
         animations = []
-        for i, elf in enumerate(self.array):
+        for i in range(len(self.array)):
             level = self._tree_level(i+1)
-            # upShift = self[i].animate.shift(UP*self.uplevel*level)
-            boxWidth = self.rectHeight*2**level + (2**level-1)*self.buffer
+            boxWidth = self[0][0].get_width()*2**level + (2**level-1)*self.style["x_buffer"]
+            tempStyle = self.style["rect"].copy()
+            tempStyle["width"] = boxWidth
+            tempStyle["height"] = self[0][0].get_height()
             newElement = (
                 TreeElement(Rectangle(
-                    height=self.rectHeight, 
-                    width=boxWidth, 
-                    color=BLUE
+                    **tempStyle
                     ), 
-                    Text(str(tree.get_tree()[i+1]), weight=SEMIBOLD)
-                    .scale(0.4)
+                    Text(str(tree.get_tree()[i+1]), **self.style["value"], fill_opacity=1 if self.show_values else 0)
+                    .scale(self.style["value_scale"])
                 )
-                .move_to(self[i].get_right(), RIGHT)
-                .shift(UP*self.uplevel*level)
+                .move_to(self[i][0].get_right(), RIGHT)
+                .shift(UP*self[0][0].get_height()*level + (UP*self.style["y_buffer"]*level))
             )
+            if self.show_indices:
+                newElement += self[i][2]
             domainExpansion = Transform(self[i], newElement)
             animations.append(domainExpansion)
 
         return animations
+    
+    def hightlight(self, index):
+        pass
 
+# @override
+# def scale(self,n):
+#     super().scale(n)
+
+#     self.style["value"]["scale"] *= n
