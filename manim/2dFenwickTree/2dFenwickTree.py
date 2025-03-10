1719330270d417dead2ce4cbb42b0f03b7eb35a5
Slug-Boi <github.unshipped609@slmail.me>
2025-03-10 13:44:43 +0100

    scale up animation and move things together
    Co-authored-by: August-Brandt <augustbrandt170@gmail.com>

--- manim/2dFenwickTree/2dFenwickTree.py
+++ manim/2dFenwickTree/2dFenwickTree.py
@@ -21,26 +21,27 @@ class TwoDFenwick(Scene):
         small_tree_style["rect"]["height"] = 0.25
         small_trees = VGroup()
         for i in range(len(M2)):
-            small_trees.add(BoxTree([0 for _ in range(len(M2[i]))], style=small_tree_style, show_indices=True, show_values=False).create().scale(0.5))
+            small_trees.add(BoxTree([0 for _ in range(len(M2[i]))], style=small_tree_style, show_indices=True, show_values=False).create().scale(0.75))
 
         small_trees.arrange(UP, buff=1)
         self.play(LaggedStart(*[FadeIn(tree) for tree in small_trees], lag_ratio=0.5))
         self.wait(0.75)
 
         big_tree_style = deepcopy(BoxTree.defaultStyle)
-        big_tree_style['x_buffer'] = (small_trees[1].get_center() - small_trees[0].get_center())[1] - big_tree_style["rect"]["height"] + small_trees[0][0][2].get_height()/3
+        big_tree_style['x_buffer'] = 1.01
+        #(small_trees[1].get_center() - small_trees[0].get_center())[1] - big_tree_style["rect"]["height"]*2
         big_tree_style["y_buffer"] = 0.4
         big_tree_style["rect"]["height"] = 0.35
+        big_tree_style["rect"]["width"] = small_trees[0].get_height()
 
-        #TODO: add a create function and change current create to animate_create in box_tree module
-        big_tree = BoxTree([0 for _ in range(len(M2))], show_indices=True, show_values=False, style=big_tree_style).create().rotate(PI/2).move_to(small_trees.get_corner(DL),DR).shift(LEFT*0.2)
+        big_tree = BoxTree([0 for _ in range(len(M2))], show_indices=True, show_values=False, style=big_tree_style).create().rotate(PI/2).move_to(small_trees.get_corner(DL),DR).shift(LEFT*0.3)
         self.play(FadeIn(big_tree))
-        self.play(big_tree.animate.to_edge(LEFT))
-        self.play(small_trees.animate.move_to(big_tree.get_corner(DR),DL).shift(RIGHT*0.2))
+        self.play(big_tree.animate.to_edge(LEFT).shift(RIGHT*1.5))
+        self.play(small_trees.animate.move_to(big_tree.get_corner(DR),DL).shift(RIGHT*0.3))
         self.wait(2)
 
-        fenwick_mat_label = Text("Fenwick Tree Matrix", weight=SEMIBOLD, font="DejaVu Sans Condensed", font_size=36).to_corner(UR)
-        dBitMatrix = Matrix(bit.BIT).scale(0.6).move_to(fenwick_mat_label.get_bottom(),UP).shift(DOWN*0.2)
+        fenwick_mat_label = Text("Fenwick Tree Matrix", weight=SEMIBOLD, font="DejaVu Sans Condensed", font_size=36).to_corner(UR).shift(LEFT*1)
+        dBitMatrix = Matrix(bit.BIT).scale(0.7).move_to(fenwick_mat_label.get_bottom(),UP).shift(DOWN*0.2)
         self.play(
             AnimationGroup(Write(fenwick_mat_label),
             Create(dBitMatrix)
@@ -49,7 +50,7 @@ class TwoDFenwick(Scene):
         self.wait(0.5)
 
         base_mat_label = Text("Base Matrix", weight=SEMIBOLD, font="DejaVu Sans Condensed", font_size=36).next_to(dBitMatrix,DOWN).shift(DOWN*0.2)
-        base_mat = Matrix(M2).scale(0.6).move_to(base_mat_label.get_bottom(),UP).shift(DOWN*0.2)
+        base_mat = Matrix(M2).scale(0.7).move_to(base_mat_label.get_bottom(),UP).shift(DOWN*0.2)
         self.play(AnimationGroup(Write(base_mat_label), 
         Create(base_mat), 
         lag_ratio=0.2))
