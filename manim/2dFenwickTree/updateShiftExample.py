1e21d106439b2291db3662bc32c6be2cf6f3805e
August-Brandt <augustbrandt170@gmail.com>
2025-03-21 14:39:45 +0100

    Change from x and y to row and col

--- manim/2dFenwickTree/updateShiftExample.py
+++ manim/2dFenwickTree/updateShiftExample.py
@@ -1,6 +1,6 @@
-while x <= matrix_size:
-    loop_y = y 
-    while loop_y <= matrix_size:
-        BIT[x][loop_y] += val
-        loop_y += (loop_y & -loop_y)
-    x += (x & -x)
+while row <= max_rows:
+    loop_col = col 
+    while loop_col <= max_cols:
+        BIT[row][loop_col] += val
+        loop_col += (loop_col & -loop_col)
+    row += (row & -row)
