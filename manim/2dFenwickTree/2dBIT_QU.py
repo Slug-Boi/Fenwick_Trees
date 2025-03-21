e9a69a275bd1c38673f645ae18ada130ad5731c7
August-Brandt <augustbrandt170@gmail.com>
2025-03-21 15:00:17 +0100

    Better code formatting

--- manim/2dFenwickTree/2dBIT_QU.py
+++ manim/2dFenwickTree/2dBIT_QU.py
@@ -25,10 +25,19 @@ class QueryUpdateTwoDBIT(Scene):
         small_tree_style["rect"]["height"] = 0.25
         small_trees = VGroup()
         for i in range(len(M2)):
-            small_trees.add(BoxTree([0 for _ in range(len(M2[i]))], style=small_tree_style, show_indices=True, show_values=False).create().scale(0.75))
+            small_trees.add(
+                BoxTree(
+                    [0 for _ in range(len(M2[i]))], 
+                    style=small_tree_style, 
+                    show_indices=True, 
+                    show_values=False
+                )
+                .create()
+                .scale(0.75)
+            )
 
         small_trees.arrange(UP, buff=1)
-        self.play(LaggedStart(*[FadeIn(tree) for tree in small_trees], lag_ratio=0.5))
+        self.play(LaggedStart(*[FadeIn(tree) for tree in small_trees], lag_ratio=0.5), run_time=0.7)
         self.wait(0.75)
 
         big_tree_style = deepcopy(BoxTree.defaultStyle)
@@ -38,26 +47,71 @@ class QueryUpdateTwoDBIT(Scene):
         big_tree_style["rect"]["height"] = 0.35
         big_tree_style["rect"]["width"] = small_trees[0].get_height()
 
-        big_tree = BoxTree([0 for _ in range(len(M2))], show_indices=True, show_values=False, style=big_tree_style).create().rotate(PI/2).move_to(small_trees.get_corner(DL),DR).shift(LEFT*0.3)
-        self.play(FadeIn(big_tree))
-        self.play(big_tree.animate.to_edge(LEFT).shift(RIGHT*0.2))
-        self.play(small_trees.animate.move_to(big_tree.get_corner(DR),DL).shift(RIGHT*0.3))
+        big_tree = (
+            BoxTree(
+                [0 for _ in range(len(M2))], 
+                show_indices=True, 
+                show_values=False, 
+                style=big_tree_style
+            )
+            .create()
+            .rotate(PI/2)
+            .move_to(small_trees.get_corner(DL),DR)
+            .shift(LEFT*0.3)
+        )
+        self.play(FadeIn(big_tree), run_time=0.7)
+        self.play(big_tree.animate.to_edge(LEFT).shift(RIGHT*0.2), run_time=0.7)
+        self.play(small_trees.animate.move_to(big_tree.get_corner(DR),DL).shift(RIGHT*0.3), run_time=0.7)
         self.wait(2)
 
-        fenwick_mat_label = Text("Fenwick Tree Matrix", weight=SEMIBOLD, font="DejaVu Sans Condensed", font_size=36).to_corner(UR).shift(LEFT*0.2)
-        dBitMatrix = Matrix(bit.BIT).scale(0.7).move_to(fenwick_mat_label.get_bottom(),UP).shift(DOWN*0.2)
+        fenwick_mat_label = (
+            Text(
+                "Fenwick Tree Matrix", 
+                weight=SEMIBOLD, 
+                font="DejaVu Sans Condensed", 
+                font_size=36
+            )
+            .to_corner(UR)
+            .shift(LEFT*0.2)
+        )
+        dBitMatrix = (
+            Matrix(bit.BIT)
+            .scale(0.7)
+            .move_to(fenwick_mat_label.get_bottom(),UP)
+            .shift(DOWN*0.2)
+        )
         self.play(
-            AnimationGroup(Write(fenwick_mat_label),
-            Create(dBitMatrix)
-            ,lag_ratio=0.2
-            ))
+            AnimationGroup(
+                Write(fenwick_mat_label),
+                Create(dBitMatrix),
+                lag_ratio=0.2
+            )
+        )
         self.wait(0.5)
 
-        base_mat_label = Text("Base Matrix", weight=SEMIBOLD, font="DejaVu Sans Condensed", font_size=36).next_to(dBitMatrix,DOWN).shift(DOWN*0.2)
-        base_mat = Matrix(M2).scale(0.7).move_to(base_mat_label.get_bottom(),UP).shift(DOWN*0.2)
-        self.play(AnimationGroup(Write(base_mat_label), 
-        Create(base_mat), 
-        lag_ratio=0.2))
+        base_mat_label = (
+            Text(
+                "Base Matrix", 
+                weight=SEMIBOLD, 
+                font="DejaVu Sans Condensed", 
+                font_size=36
+            )
+            .next_to(dBitMatrix,DOWN)
+            .shift(DOWN*0.2)
+        )
+        base_mat = (
+            Matrix(M2)
+            .scale(0.7)
+            .move_to(base_mat_label.get_bottom(),UP)
+            .shift(DOWN*0.2)
+        )
+        self.play(
+            AnimationGroup(
+                Write(base_mat_label), 
+                Create(base_mat), 
+                lag_ratio=0.2
+            )
+        )
         self.wait(0.5)
 
         """
@@ -65,12 +119,60 @@ class QueryUpdateTwoDBIT(Scene):
         """
 
         # Prepare the mother of all text objects...
-        fullQuery = Text("Query(2,1)", font="DejaVu Sans Condensed", font_size=36, weight=SEMIBOLD).to_edge(UP).shift(DOWN*0.3+LEFT*0.2)
-        queryText = Text("Sub-Query: (", font="DejaVu Sans Condensed", font_size=30).to_edge(UP).shift(DOWN*1.5+LEFT*0.8)
-        comma = Text(",", font="DejaVu Sans Condensed", font_size=30).next_to(queryText,RIGHT)
-        endPar = Text(")", font="DejaVu Sans Condensed", font_size=30).next_to(comma,RIGHT)
-        partialSums = Text("Partial Sums: [", font="DejaVu Sans Condensed", font_size=30).move_to(queryText.get_bottom(),UP).align_to(queryText,LEFT).shift(DOWN*0.4)
-        endBracket = Text("]", font="DejaVu Sans Condensed", font_size=30).next_to(partialSums,RIGHT)
+        fullQuery = (
+            Text(
+                "Query(2,1)", 
+                font="DejaVu Sans Condensed", 
+                font_size=36, 
+                weight=SEMIBOLD
+            )
+            .to_edge(UP)
+            .shift(DOWN*0.3+LEFT*0.2)
+        )
+        queryText = (
+            Text(
+                "Sub-Query: (", 
+                font="DejaVu Sans Condensed", 
+                font_size=30
+            )
+            .to_edge(UP)
+            .shift(DOWN*1.5+LEFT*0.8)
+        )
+        comma = (
+            Text(
+                ",", 
+                font="DejaVu Sans Condensed", 
+                font_size=30
+            )
+            .next_to(queryText,RIGHT)
+        )
+        endPar = (
+            Text(
+                ")", 
+                font="DejaVu Sans Condensed", 
+                font_size=30
+            )
+            .next_to(comma,RIGHT)
+        )
+        partialSums = (
+            Text(
+                "Partial Sums: [", 
+                font="DejaVu Sans Condensed", 
+                font_size=30
+            )
+            .move_to(queryText.get_bottom(),UP)
+            .align_to(queryText,LEFT)
+            .shift(DOWN*0.4)
+        )
+        endBracket = (
+            Text(
+                "]", 
+                font="DejaVu Sans Condensed", 
+                font_size=30
+            )
+            .next_to(partialSums,RIGHT)
+        )
+
 
         # Write static text items 
         ops = bit.getSumPositions(2, 1)
@@ -95,12 +197,29 @@ class QueryUpdateTwoDBIT(Scene):
                 # Cleanup for next run
                 self.play(FadeOut(rect_highlight), FadeOut(x), FadeOut(y))
 
-            x = Text(str(query[0]), font="DejaVu Sans Condensed", font_size=30).next_to(queryText,RIGHT,buff=0.15)
+            x = (
+                Text(
+                    str(query[0]), 
+                    font="DejaVu Sans Condensed", 
+                    font_size=30
+                )
+                .next_to(queryText,RIGHT,buff=0.15)
+            )
             animations.append(Write(x))
 
-            animations.append(Write(comma.next_to(x,RIGHT,buff=0.15).align_to(x,DOWN)) if y is None else comma.animate.next_to(x,RIGHT,buff=0.15).align_to(x,DOWN))
+            animations.append(
+                Write(comma.next_to(x,RIGHT,buff=0.15).align_to(x,DOWN)) if y is None else comma.animate.next_to(x,RIGHT,buff=0.15).align_to(x,DOWN)
+            )
 
-            y = Text(str(query[1]), font="DejaVu Sans Condensed", font_size=30).next_to(comma,RIGHT,buff=0.15).align_to(x,DOWN)
+            y = (
+                Text(
+                    str(query[1]), 
+                    font="DejaVu Sans Condensed", 
+                    font_size=30
+                )
+                .next_to(comma,RIGHT,buff=0.15)
+                .align_to(x,DOWN)
+            )
             animations.append(Write(y))
 
             self.play(endPar.animate.next_to(y,RIGHT,buff=0.15))
@@ -109,25 +228,55 @@ class QueryUpdateTwoDBIT(Scene):
             self.wait(0.5)
 
             # Create bounding rects in the Fenwick Tree matrix
-            rect_highlight = SurroundingRectangle(dBitMatrix.get_mob_matrix()[query[0]][query[1]], color=GREEN, buff=0.1)
+            rect_highlight = SurroundingRectangle(
+                dBitMatrix.get_mob_matrix()[query[0]][query[1]], 
+                color=GREEN, 
+                buff=0.1
+            )
             self.play(Create(rect_highlight))
 
             # Highlight BIT value in the Fenwick Tree matrix
-            self.play(big_tree[query[0]-1].animate.highlight(GREEN),
-                      small_trees[query[0]-1][query[1]-1].animate.highlight(GREEN),
-                      run_time=0.5
-                      )
+            self.play(
+                big_tree[query[0]-1].animate.highlight(GREEN),
+                small_trees[query[0]-1][query[1]-1].animate.highlight(GREEN),
+                run_time=0.5
+            )
             self.wait(0.25)
 
             # add sum to partial sums
-            sumText = Text(str(query[2]), font="DejaVu Sans Condensed", font_size=30).move_to(partialSums.get_center(),ORIGIN).align_to(partialSums,RIGHT).shift(RIGHT*0.58)
+            sumText = (
+                Text(
+                    str(query[2]), 
+                    font="DejaVu Sans Condensed", 
+                    font_size=30
+                )
+                .move_to(partialSums.get_center(),ORIGIN)
+                .align_to(partialSums,RIGHT)
+                .shift(RIGHT*0.58)
+            )
             sums_list.append(sumText)
 
             # Pre-move the end bracket to the right of the sum text to avoid overlap but keep y alignment
             if len(sums_list) > 1:
                 sumText.move_to(sums_list[-1].get_bottom(),UP).align_to(sums_list[-1],LEFT).shift(DOWN*0.2)
-                comma_list.append(Text(",", font="DejaVu Sans Condensed", font_size=30).next_to(sums_list[-2],RIGHT,buff=0.15).align_to(sums_list[-2],DOWN))
-                self.play(AnimationGroup(endBracket.animate.next_to(sums_list[-1],RIGHT,buff=0.15).align_to(sums_list[-1],DOWN).shift(DOWN*0.05),Write(comma_list[-1]), lag_ratio=0.6))
+                comma_list.append(
+                    Text(
+                        ",", 
+                        font="DejaVu Sans Condensed", 
+                        font_size=30
+                    )
+                    .next_to(sums_list[-2],RIGHT,buff=0.15)
+                    .align_to(sums_list[-2],DOWN)
+                )
+                self.play(
+                    AnimationGroup(
+                        endBracket.animate.next_to(sums_list[-1],RIGHT,buff=0.15)
+                            .align_to(sums_list[-1],DOWN)
+                            .shift(DOWN*0.05),
+                        Write(comma_list[-1]), 
+                        lag_ratio=0.6
+                    )
+                )
                 self.play(Write(sumText))
             else:  
                 self.play(endBracket.animate.next_to(sumText,RIGHT,buff=0.15).align_to(partialSums,DOWN))
@@ -138,11 +287,38 @@ class QueryUpdateTwoDBIT(Scene):
             
 
         # Final cleanup and write sum
-        finalSumText = Text("Sum: ", font="DejaVu Sans Condensed", weight=SEMIBOLD,font_size=30).move_to(sums_list[-1].get_bottom(),UP).align_to(partialSums,LEFT).shift(DOWN*0.9)
-        finalSum = Text(str(ops[0]), font="DejaVu Sans Condensed", weight=SEMIBOLD, font_size=30).next_to(finalSumText,RIGHT,buff=0.15).align_to(finalSumText,DOWN)
+        finalSumText = (
+            Text(
+                "Sum: ", 
+                font="DejaVu Sans Condensed", 
+                weight=SEMIBOLD,
+                font_size=30
+            )
+            .move_to(sums_list[-1].get_bottom(),UP)
+            .align_to(partialSums,LEFT)
+            .shift(DOWN*0.9)
+        )
+        finalSum = (
+            Text(
+                str(ops[0]), 
+                font="DejaVu Sans Condensed", 
+                weight=SEMIBOLD, 
+                font_size=30
+            )
+            .next_to(finalSumText,RIGHT,buff=0.15)
+            .align_to(finalSumText,DOWN)
+        )
 
-        self.play(AnimationGroup(Write(finalSumText), Write(finalSum), lag_ratio=0.4))
+        self.play(
+            AnimationGroup(
+                Write(finalSumText), 
+                Write(finalSum), 
+                lag_ratio=0.4
+            )
+        )
         self.wait(2)
+        
+        # Clean up
         self.play(Unwrite(fullQuery), 
                   Unwrite(queryText),
                   Unwrite(comma), 
