df2d13015f539c52318e75611c9118c2aea49d17
August-Brandt <augustbrandt170@gmail.com>
2025-05-02 13:20:16 +0200

    (lambda: print("Success in the face of failure")()
    Co-authored-by: Slug-Boi <theis.p.holm@gmail.com>

--- manim/heaps/Heap.py
+++ manim/heaps/Heap.py
@@ -1,45 +1,148 @@
 from manim import *
 from manim_dsa import *
 from heapq import *
+from math import floor, log2
+from copy import copy
 global_font = "DejaVu Sans Condensed"
 arr = [3, 2, -3, 6, 5, 4, -2, 7]
 
-
 class FenwickTree_Tree(Scene):
-       def construct(self):
-        UPLEVEL = 2
-        heapify(arr)
+    @staticmethod
+    def getLevel(index):
+        return floor(log2(index + 1))
 
+    def construct(self):
+        DOWNLEVEL = 2
+        SIDELEVEL = 1.5
+        originalArray = list(map(str, copy(arr)))
+        heapify(arr)
+        print(arr)
         edges = {}
+        node_positions = dict()
 
         for i in range(len(arr)):
             if i == 0:
-                continue
-            parent = (i - 1) // 2
-            edges[i] = parent
+                node_positions[str(arr[0])] = UP*3.2
+            else:
+                level = self.getLevel(i)
+                if level == 1:
+                    level_adjust = 1.2
+                else:
+                    level_adjust = 1.5
+                if i % 2 == 1:
+                    node_positions[str(arr[i])] = node_positions[str(arr[i // 2])] + DOWN * DOWNLEVEL + LEFT * ((SIDELEVEL / level)*level_adjust)
+                else:
+                    node_positions[str(arr[i])] = node_positions[str(arr[(i-1) // 2])] + DOWN * DOWNLEVEL + RIGHT * ((SIDELEVEL / level) * level_adjust)
+            parent = i
             child1 = 2 * i + 1
             child2 = 2 * i + 2
+            edges[str(arr[parent])] = []
             if child1 < len(arr):
-                edges[child1] = i
+                edges[str(arr[parent])].append(str(arr[child1]))
             if child2 < len(arr):
-                edges[child2] = i
-                
+                edges[str(arr[parent])].append(str(arr[child2]))
 
+        print(edges)
+
+        heapText = Text(
+            "Min Binary Heap",
+            weight=SEMIBOLD,
+            # font=global_font
+        ).to_corner(UL).scale(0.8)
+        inputArrayText = Text(
+            "Input: [" + ", ".join(originalArray) + "]",
+        ).move_to(heapText.get_bottom(), UP).scale(0.5).align_to(heapText.get_left(), LEFT)
+
+        self.play(Write(heapText))
+        self.wait(0.2)
+
+        self.play(Write(inputArrayText), run_time=0.7)
+        self.wait(0.2)
+        graph = MGraph(edges, nodes_position=node_positions, style=MGraphStyle.PURPLE)
+        # graph.scale(0.5)
+        self.play(Create(graph), run_time=1.5)
+        self.wait(1)
         
-
-        graph = MGraph(arr, edges, vertex_config={"radius": 0.3, "color": BLUE},)
-        graph.move_to(ORIGIN)
-        graph.scale(0.5)
-        self.add(graph)
+        self.play(Unwrite(inputArrayText), run_time=0.7)
         self.wait(0.5)
 
-        # Add edges
-        for edge in edges:
-            start = graph.vertices[edge[0]].get_center()
-            end = graph.vertices[edge[1]].get_center()
-            line = Line(start, end, color=WHITE)
-            self.add(line)
-            self.wait(0.5)
+        """
+        Insert Animation
+        """
+        heapArrayText = Text(
+            "Heap: [" + ", ".join(list(map(str, copy(arr)))) + "]"
+        ).move_to(heapText.get_bottom(), UP).scale(0.5).align_to(heapText.get_left(), LEFT)
+        self.play(Write(heapArrayText), run_time=0.7)
 
+        insertText = Text(
+            "Insert(1)"
+        ).move_to(heapArrayText.get_bottom(),UP).scale(0.5).align_to(heapArrayText.get_left(), LEFT)
+        self.play(Write(insertText), run_time=0.7)
 
+        self.play(
+            graph.animate.add_node(
+                "1",
+                # ORIGIN
+                node_positions["6"] + DOWN * DOWNLEVEL + RIGHT * ((SIDELEVEL / 3) * 1.5)
+            ),
+            run_time = 0.7
+        )
+        self.play(
+            graph.animate.add_edge(
+                "6",
+                "1"
+            ),
+            run_time = 0.7
+        )
+        self.wait(0.5)
+        Text.become(graph[str(6)].submobjects[1], Text("1", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(6)].submobjects[1].get_bottom(), DOWN))
+        Text.become(graph[str(1)].submobjects[1], Text("6", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(1)].submobjects[1].get_bottom(), DOWN))
+        self.play(Write(graph[str(6)].submobjects[1]), Write(graph[str(1)].submobjects[1]))
+        
+        self.wait(0.7)
+        Text.become(graph[str(2)].submobjects[1], Text("1", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(2)].submobjects[1].get_bottom(), DOWN))
+        Text.become(graph[str(6)].submobjects[1], Text("2", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(6)].submobjects[1].get_bottom(), DOWN))
+        self.play(Write(graph[str(6)].submobjects[1]), Write(graph[str(2)].submobjects[1]))
+        self.wait(0.2)
+        
+        self.play(FadeOut(heapArrayText), run_time=0.7)
+        heapArrayText.become(
+            Text(
+                "Heap: [" + ", ".join(list(map(str, copy((lambda: heapify((lambda: arr.append(1) or arr)()) or arr)())))) + "]"
+            ).scale(0.5).move_to(heapArrayText.get_left(), LEFT)
+        )
+        self.play(FadeIn(heapArrayText), run_time=0.7)
+        self.wait(0.7)
+        self.play(FadeOut(insertText), run_time=0.7)
+
+        self.wait(1.5)
+
+        """
+        Remove animation
+        """
+        removeText = Text(
+            "Remove(-2)"
+        ).move_to(heapArrayText.get_bottom(),UP).scale(0.5).align_to(heapArrayText.get_left(), LEFT)
+        self.play(Write(removeText), run_time=0.7)
+        self.wait(0.7)
+
+        self.play(Unwrite(graph[str(-2)].submobjects[1]))
+        self.wait(0.5)
+
+        self.play(Unwrite(graph[str(1)].submobjects[1]), Uncreate(graph[str(1)]), FadeOut(graph.edges[("6", "1")]))
+        self.wait(0.5)
+
+        Text.become(graph[str(-2)].submobjects[1], Text("6", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(-2)].submobjects[1].get_bottom(), DOWN))
+        self.play(Write(graph[str(-2)].submobjects[1]))
+        self.wait(0.7)
+        
+        Text.become(graph[str(-2)].submobjects[1], Text("3", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(-2)].submobjects[1].get_bottom(), DOWN))
+        Text.become(graph[str(3)].submobjects[1], Text("6", **MGraphStyle.DEFAULT.node_label).move_to(graph[str(3)].submobjects[1].get_bottom(), DOWN))
+        self.play(Write(graph[str(-2)].submobjects[1]), Write(graph[str(3)].submobjects[1]))
+        
+        self.wait(0.7)
+
+        self.play(FadeOut(removeText), run_time=0.7)
+
+        self.wait(1.5)
     
